527243c6c4a8314cf126f01e3364c824bf243063
ElmazMed <elmazgour.mohamed@gmail.com>
2024-09-07 01:54:35 +0100

    cards created start working on seach bar

---
+++ src/components/Cards.jsx
@@ -0,0 +1,104 @@
+import { FaChartColumn } from "react-icons/fa6";
+import { FaChartPie } from "react-icons/fa";
+import { FaUsers } from "react-icons/fa";
+import { FaPercentage } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa6";
+import { FaArrowUp } from "react-icons/fa6";
+
+export default function Cards() {
+  return (
+    <>
+      <section className="bg-sky-500 h-96 w-screen">
+        <div className="cards md:flex justify-evenly items-center">
+          <div className="card bg-white shadow-md p-4 rounded-md w-72">
+            <div className="card-body">
+              <div className="flex justify-between mb-5">
+                <div>
+                  <h5 className="uppercase text-gray-500">Traffic</h5>
+                  <span>350,896</span>
+                </div>
+                <div className="rounded-full w-12 h-12 p-2 bg-red-600 flex items-center justify-center">
+                  <FaChartColumn className="text-white text-lg" />
+                </div>
+              </div>
+
+              <p className="flex gap-2 text-gray-500">
+                <span className="flex items-center text-green-500">
+                  <FaArrowUp />
+                  3.48%
+                </span>{" "}
+                Since last month
+              </p>
+            </div>
+          </div>
+
+          <div className="card bg-white shadow-md p-4 rounded-md w-72">
+            <div className="card-body">
+              <div className="flex justify-between mb-5">
+                <div>
+                  <h5 className="uppercase text-gray-500">New users</h5>
+                  <span>2,356</span>
+                </div>
+                <div className="rounded-full w-12 h-12 p-2 bg-orange-500 flex items-center justify-center">
+                  <FaChartPie className="text-white text-lg" />
+                </div>
+              </div>
+
+              <p className="flex gap-2 text-gray-500">
+                <span className="flex items-center text-red-500">
+                  <FaArrowUp />
+                  3.48%
+                </span>{" "}
+                Since last week
+              </p>
+            </div>
+          </div>
+
+          <div className="card bg-white shadow-md p-4 rounded-md w-72">
+            <div className="card-body">
+              <div className="flex justify-between mb-5">
+                <div>
+                  <h5 className="uppercase text-gray-500">Sales</h5>
+                  <span>929</span>
+                </div>
+                <div className="rounded-full w-12 h-12 p-2 bg-yellow-400 flex items-center justify-center">
+                  <FaUsers className="text-white text-lg" />
+                </div>
+              </div>
+
+              <p className="flex gap-2 text-gray-500">
+                <span className="flex items-center text-red-500">
+                  <FaArrowUp />
+                  1.10%
+                </span>{" "}
+                Since yesterday
+              </p>
+            </div>
+          </div>
+
+          <div className="card bg-white shadow-md p-4 rounded-md w-72">
+            <div className="card-body">
+              <div className="flex justify-between mb-5">
+                <div>
+                  <h5 className="uppercase text-gray-500">Performance</h5>
+                  <span>49,15%</span>
+                </div>
+                <div className="rounded-full w-12 h-12 p-2 bg-cyan-400 flex items-center justify-center">
+                  <FaPercentage className="text-white text-lg" />
+                </div>
+              </div>
+
+              <p className="flex gap-2 text-gray-500">
+                <span className="flex items-center text-green-500">
+                  <FaArrowUp />
+                  12%
+                </span>{" "}
+                Since last month
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,56 +5,62 @@ import { FaUser } from "react-icons/fa";
 import { FaListUl } from "react-icons/fa6";
 import { MdOutlineContactSupport } from "react-icons/md";
 import { IoIosRocket } from "react-icons/io";
+import Cards from "../components/Cards";
 
 export default function Home() {
   return (
     <>
-      <nav className="bg-white shadow-md h-screen  w-56  flex flex-col justify-between">
-        <div className="p-8">
-          <div className="logo mb-20">
-            <h1 className="text-3xl text-sky-500 font-bold uppercase">
-              Admin.
-            </h1>
+      <main className="flex">
+        <nav className="bg-white shadow-md h-screen  w-72  flex flex-col justify-between">
+          <div className="p-8">
+            <div className="logo mb-20">
+              <h1 className="text-3xl text-sky-500 font-bold uppercase">
+                Admin.
+              </h1>
+            </div>
+            <ul className="flex flex-col gap-12">
+              <Link className="flex items-center gap-4 text-sky-500">
+                <CgScreen />
+                <li className="text-gray-500 hover:text-black transition-all">
+                  Dashboard
+                </li>
+              </Link>
+              <Link className="flex items-center gap-4 text-amber-400">
+                <FaUser />
+
+                <li className="text-gray-500 hover:text-black transition-all">
+                  User Profile
+                </li>
+              </Link>
+              <Link className="flex items-center gap-4 text-red-600">
+                <FaListUl />
+
+                <li className="text-gray-500 hover:text-black transition-all">
+                  Tables
+                </li>
+              </Link>
+
+              <Link className="flex items-center gap-4 text-indigo-600">
+                <MdOutlineContactSupport />
+
+                <li className="text-gray-500 hover:text-black transition-all">
+                  Support
+                </li>
+              </Link>
+            </ul>
           </div>
-          <ul className="flex flex-col gap-12">
-            <Link className="flex items-center gap-4 text-sky-500">
-              <CgScreen />
-              <li className="text-gray-500 hover:text-black transition-all">
-                Dashboard
+          <div className="bg-gray-50 p-5">
+            <Link className="flex items-center gap-4 text-violet-700">
+              <IoIosRocket />
+
+              <li className="list-none font-light text-black">
+                Upgrade to PRO
               </li>
             </Link>
-            <Link className="flex items-center gap-4 text-amber-400">
-              <FaUser />
-
-              <li className="text-gray-500 hover:text-black transition-all">
-                User Profile
-              </li>
-            </Link>
-            <Link className="flex items-center gap-4 text-red-600">
-              <FaListUl />
-
-              <li className="text-gray-500 hover:text-black transition-all">
-                Tables
-              </li>
-            </Link>
-
-            <Link className="flex items-center gap-4 text-indigo-600">
-              <MdOutlineContactSupport />
-
-              <li className="text-gray-500 hover:text-black transition-all">
-                Support
-              </li>
-            </Link>
-          </ul>
-        </div>
-        <div className="bg-gray-50 p-5">
-          <Link className="flex items-center gap-4 text-violet-700">
-            <IoIosRocket />
-
-            <li className="list-none font-light text-black">Upgrade to PRO</li>
-          </Link>
-        </div>
-      </nav>
+          </div>
+        </nav>
+        <Cards />
+      </main>
     </>
   );
 }
