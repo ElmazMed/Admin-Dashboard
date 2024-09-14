79a1010f3f085d7301260209e280e016124735b0
ElmazMed <elmazgour.mohamed@gmail.com>
2024-09-14 22:20:45 +0100

    fixing nav menu err

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ import { FaListUl } from "react-icons/fa6";
 import { MdOutlineContactSupport } from "react-icons/md";
 import { IoIosRocket } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
+import { IoSearch } from "react-icons/io5";
 import Cards from "../components/Cards";
 import Dashboard from "./Dashboard";
 import user from "../assets/pexels-justin-shaifer-501272-1222271.jpg";
@@ -14,8 +16,8 @@ export default function Home() {
   return (
     <>
       <main className="sm:flex">
-        <div className=" mobile-menu bg-white w-full p-6 flex justify-between items-center">
-          <button className="text-3xl" type="button">
+        <div className=" mobile-menu bg-white w-full p-6 flex justify-between sm:hidden items-center">
+          <button className="text-3xl text-gray-400" type="button">
             <RxHamburgerMenu />
           </button>
           <div className="logo">
@@ -30,13 +32,27 @@ export default function Home() {
           </button>
         </div>
 
-        <nav className="sm:overflow-y-auto sm:w-2/12 w-11/12 sm:mr-10 absolute top-6 left-0 right-0 mx-auto sm:relative">
+        <nav className="sm:overflow-y-auto sm:w-2/12 w-11/12 sm:mr-10 absolute sm:top-0 top-6 left-0 right-0 mx-auto sm:relative">
           <div className="sm:fixed flex flex-col sm:w-2/12 w-full justify-between bg-white shadow-md sm:h-screen">
             <div className="sm:p-8 p-4">
-              <div className="logo sm:mb-20 mb-7">
-                <h1 className="text-3xl text-center text-sky-500 font-bold uppercase">
+              <div className="logo sm:mb-20 flex justify-between sm:block mb-7">
+                <h1 className="text-3xl sm:text-center text-sky-500 font-bold uppercase">
                   Admin.
                 </h1>
+                <button
+                  className="text-3xl sm:hidden text-gray-400"
+                  type="button"
+                >
+                  <AiOutlineClose />
+                </button>
+              </div>
+              <div className="mobile-search sm:hidden flex justify-between items-center bg-white border-2 border-gray-300 rounded-sm w-full p-2 mb-8">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="bg-transparent border-none outline-none"
+                />
+                <IoSearch className="text-2xl text-gray-400" />
               </div>
               <ul className="flex flex-col gap-12">
                 <Link
